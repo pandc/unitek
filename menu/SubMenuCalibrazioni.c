7a0bd0fa5d7de7ed2e755a43b2dd8d2d6361c067
Diego Colombo <diegocolombotrezzo@gmail.com>
2016-07-19 10:37:55 +0200

    non più usato,rimuovere

---
+++ menu/SubMenuCalibrazioni.c
@@ -0,0 +1,21 @@
+//calibrazione PT100
+
+
+
+#include "bsp.h"
+#include "io.h"
+
+#include "freertos.h"
+#include "task.h"
+
+#include "my_definitions.h"
+#include "Display_128x64.h"
+#include "SSD1322.h"
+#include "BitmapsImmagini.h"
+#include "Conversioni.h"
+#include "Menu.h"
+#include "keyboard.h"
+#include "VariabiliGlobali_di_Lavoro.h"
+
+
+
