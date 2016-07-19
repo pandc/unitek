9223bc59269887117bfc2e86d60a2cef60f450c3
Diego Colombo <diegocolombotrezzo@gmail.com>
2016-07-19 10:18:49 +0200

    modifiche poi commentate per prove i2c con Cesare

--- drivers/i2c.h
+++ drivers/i2c.h
@@ -2,7 +2,7 @@
 #define	__I2C_H
 
 #define MIN_I2C_FREQ		1000
-#define MAX_I2C_FREQ		400000
+#define MAX_I2C_FREQ		400000//era 400000 Diego 14/07
 
 void I2C_Open(uint32_t freq);
 int I2C_RandWrite(uint8_t slave,uint16_t addr,uint8_t addrlen,const void *pbuffer, uint16_t len);
