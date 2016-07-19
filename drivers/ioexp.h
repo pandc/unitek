958c7f354145f3814a329bde804d76b0def42e3b
Diego Colombo <diegocolombotrezzo@gmail.com>
2016-07-19 10:21:15 +0200

    aggiunte definizioni per comandare in un colpo(disattivare)tutte le uscite concentrazione o temperatura

--- drivers/ioexp.h
+++ drivers/ioexp.h
@@ -1,14 +1,17 @@
 #ifndef __IOEXP_H
 #define __IOEXP_H
 
-#define IOEXP0_BUZZER			(1 << 0)
-#define IOEXP0_TANK_ALARM		(1 << 1)
+#define IOEXP0_BUZZER		(1 << 0)
+#define IOEXP0_TANK_ALARM	(1 << 1)
 #define IOEXP0_MIN_CONC_ALARM	(1 << 2)
-#define IOEXP0_PUMP_ENABLE		(1 << 3)
+#define IOEXP0_PUMP_ENABLE	(1 << 3)
 #define IOEXP0_MAX_CONC_ALARM	(1 << 4)
 #define IOEXP0_MIN_TEMP_ALARM	(1 << 5)
 #define IOEXP0_HEATER_ENABLE	(1 << 6)
 #define IOEXP0_MAX_TEMP_ALARM	(1 << 7)
+
+#define IOEXP0_CONC_MASK       (IOEXP0_TANK_ALARM | IOEXP0_MIN_CONC_ALARM | IOEXP0_PUMP_ENABLE | IOEXP0_MAX_CONC_ALARM)
+#define IOEXP0_TEMP_MASK       ( IOEXP0_MIN_TEMP_ALARM | IOEXP0_HEATER_ENABLE | IOEXP0_MAX_TEMP_ALARM)
 
 uint8_t IOEXP_get(void);
 void IOEXP_set(uint8_t mask);
